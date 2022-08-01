38ed455d52fb56526115e10daa9585707b146335
ananthanunni <ananthanunni@gmail.com>
2022-08-01 21:01:04 +0530

    2 files modified, 1 added

--- src/components/staggered-list/staggered-list.component.ts
+++ src/components/staggered-list/staggered-list.component.ts
@@ -1,15 +1,79 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-staggered-list',
   templateUrl: './staggered-list.component.html',
-  styleUrls: ['./staggered-list.component.css']
+  styleUrls: ['./staggered-list.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        // each time the binding value changes
+        query(
+          ':leave',
+          [
+            stagger(100, [
+              animate(
+                '300ms',
+                style({ opacity: 0, transform: 'translateY(30px)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateX(30px)' }),
+            stagger(100, [
+              animate(
+                '150ms',
+                style({ opacity: 1, transform: 'translateX(0)' })
+              ),
+            ]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
-export class StaggeredListComponent implements OnInit {
+export class StaggeredListComponent {
+  private lastId = 0;
 
-  constructor() { }
+  list: ListItem[] = [];
 
-  ngOnInit() {
+  onSubmit($event: Event): void {
+    const inputElement = $event.target as HTMLInputElement;
+
+    this.addItem(inputElement.value);
+
+    inputElement.value = '';
   }
 
+  addItems(count: number): void {
+    for (let i = 0; i < count; i++) {
+      this.addItem(`Generated Item ${this.lastId + 1}`);
+    }
+  }
+
+  onDeleteClicked(item: ListItem): void {
+    const index = this.list.findIndex((r) => r.id === item.id);
+    this.list.splice(index, 1);
+  }
+
+  private addItem(value: string): void {
+    this.list.push(new ListItem(++this.lastId, value));
+  }
+}
+
+class ListItem {
+  constructor(public id: number, public label: string) {}
 }
