82b74e085fc8993509346294ed9ac89d05c68fc8
ananthanunni <ananthanunni@gmail.com>
2022-08-01 16:31:47 +0530

    3 files modified

--- src/components/simple-enter-leave/simple-enter-leave.component.ts
+++ src/components/simple-enter-leave/simple-enter-leave.component.ts
@@ -1,8 +1,27 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+const enterTransition = transition(':enter', [
+  style({ opacity: 0, transform: 'scale(0.6)' }),
+  animate('500ms', style({ opacity: 1, transform: 'scale(1)' })),
+]);
+
+const leaveTransition = transition(':leave', [
+  animate('500ms', style({ opacity: 0, transform: 'scale(0.6)' })),
+]);
 
 @Component({
   selector: 'app-simple-enter-leave',
   templateUrl: './simple-enter-leave.component.html',
   styleUrls: ['./simple-enter-leave.component.css'],
+  animations: [trigger('targetRectangle', [enterTransition, leaveTransition])],
 })
-export class SimpleEnterLeaveComponent {}
+export class SimpleEnterLeaveComponent {
+  targetVisible = false;
+}
