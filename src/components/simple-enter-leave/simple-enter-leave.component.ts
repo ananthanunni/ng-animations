286926d808defe030601e53369ec1aecafa3bb91
ananthanunni <ananthanunni@gmail.com>
2022-08-10 18:51:57 +0530

    1 file modified

--- src/components/simple-enter-leave/simple-enter-leave.component.ts
+++ src/components/simple-enter-leave/simple-enter-leave.component.ts
@@ -1,20 +1,24 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
-const enterTransition = transition(':enter', [
-  style({ opacity: 0, transform: 'scale(0.6)' }),
-  animate('300ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
-]);
-
-const leaveTransition = transition(':leave', [
-  animate('300ms ease-out', style({ opacity: 0, transform: 'scale(0.6)' })),
-]);
-
 @Component({
   selector: 'app-simple-enter-leave',
   templateUrl: './simple-enter-leave.component.html',
   styleUrls: ['./simple-enter-leave.component.css'],
-  animations: [trigger('targetRectangle', [enterTransition, leaveTransition])],
+  animations: [
+    trigger('targetRectangle', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.6)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-out',
+          style({ opacity: 0, transform: 'scale(0.6)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class SimpleEnterLeaveComponent {
   logs: string[] = [];
