da6dce936357364e1820ac33b3028155b6613f9f
ananthanunni <ananthanunni@gmail.com>
2022-08-11 14:20:11 +0530

    2 files modified

--- src/components/complex-container/query-group-complex/query-group-complex.component.ts
+++ src/components/complex-container/query-group-complex/query-group-complex.component.ts
@@ -1,4 +1,11 @@
-import { query, trigger } from '@angular/animations';
+import {
+  animate,
+  query,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,21 +13,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './query-group-complex.component.html',
   styleUrls: ['./query-group-complex.component.css'],
   animations: [
-    trigger('animatedList', [
-      //query(:)
+    trigger('animatedContainer', [
+      state('false', style({ opacity: 0 })),
+      transition('* => true', [
+        query(':enter', [animate('1200ms', style({ color: 'red' }))], {
+          optional: true,
+        }),
+      ]),
     ]),
   ],
 })
 export class QueryGroupComplexComponent {
-  elements: number[] = [];
-
-  appendElements() {
-    const lastElement =
-      this.elements.length > 0 ? this.elements[this.elements.length - 1] : 0;
-
-    const newElements: number[] = Array(6)
-      .fill(1)
-      .map((_, index) => lastElement + 1 + index);
-    this.elements.push(...newElements);
-  }
+  isVisible = false;
 }
